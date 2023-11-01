71f8083895476f5682ed51f663a5dac86c5b032e
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2023-11-01 11:20:38 +0300

    create transfer card

--- src/pages/nft/[id].tsx
+++ src/pages/nft/[id].tsx
@@ -1,30 +1,32 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { useNFT, useValidDirectListings } from "@thirdweb-dev/react";
+import { useNFT, useValidDirectListings, useAddress } from "@thirdweb-dev/react";
 
 import Layout from "@/layout/Layout";
 import NFTDetails from "@/components/NFTDetails";
 import SellNFTCard from "@/components/SellNFTCard";
 import CancelSellingCard from "@/components/CancelSelling";
+import TransferNFTCard from "@/components/TransferNFTCard";
 import { getMarketplaceContract, getNFTContract } from "@/util/getContracts";
 
 
 function NFTDetailsPage() {
     const router = useRouter();
+    const address = useAddress();
     const { nft_contract } = getNFTContract();
     const { marketplace } = getMarketplaceContract();
     const { data: directListings } = useValidDirectListings(marketplace, {
         start: 0,
         count: 100,
     });
-    
+
     const [nftID, setNftID] = useState("");
     const [price, setPrice] = useState(0.01);
     const [symbol, setSymbol] = useState("");
     const [listingID, setListingID] = useState("");
 
     const { data: nft, isLoading: isNFTLoading } = useNFT(nft_contract, nftID);
-    
+
     useEffect(() => {
         if (typeof window !== "undefined") {
             const { id } = router.query;
@@ -44,33 +46,40 @@ function NFTDetailsPage() {
                 <h1 className="text-6xl font-semibold my-4 text-center">
                     NFT Details
                 </h1>
-
-                {isNFTLoading || !nft ? (
-                    <div className="text-center">
-                        {`Loading NFT with id ${nftID} `}
-                    </div>
-                ) : (
-                    <>
-                        <NFTDetails {...nft} />
-                        {
-                            listingID ? (
-                                <CancelSellingCard
-                                    price={price}
-                                    symbol={symbol}
-                                    listingID={listingID}
-                                />
-                            ) : (
-                                <SellNFTCard
-                                    price={price}
-                                    onUpdatePrice={setPrice}
-                                    id={nftID}
-                                />
-                            )
-                        }
-                    </>
-                )}
+                {
+                    isNFTLoading || !nft ? (
+                        <div className="text-center">
+                            {`Loading NFT with id ${nftID} `}
+                        </div>
+                    ) : (
+                        <>
+                            <NFTDetails {...nft} />
+                            {
+                                nft.owner === address && (
+                                    <>
+                                        <TransferNFTCard id={nftID} />
+                                        {
+                                            listingID ? (
+                                                <CancelSellingCard
+                                                    price={price}
+                                                    symbol={symbol}
+                                                    listingID={listingID}
+                                                />
+                                            ) : (
+                                                <SellNFTCard
+                                                    price={price}
+                                                    onUpdatePrice={setPrice}
+                                                    id={nftID}
+                                                />
+                                            )
+                                        }
+                                    </>
+                                )
+                            }
+                        </>
+                    )}
             </div>
-        </Layout>
+        </Layout >
     );
 }
 export default NFTDetailsPage;
